cd21f7be1169d4f567429a850c7124c85bbca0f6
Biran <bndiaye@isi-mtl.com>
2021-06-23 12:12:32 -0400

    add new Employee

--- webApp/src/main/java/com/example/demosringappweb/controller/EmployeeController.java
+++ webApp/src/main/java/com/example/demosringappweb/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.example.demosringappweb.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -47,7 +44,8 @@ public class EmployeeController {
      * add an employee
      */
     @GetMapping("/addEmployee")
-    public String addEmployee(){
+    public String addEmployee(Model model){
+        model.addAttribute("NewEmployee",new Employee());
         return "addEmployee";
     }
     /**

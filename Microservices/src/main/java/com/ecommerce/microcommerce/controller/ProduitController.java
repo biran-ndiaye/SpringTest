eafcbf4a693773d7144659a936a42a45503b6080
Biran <bndiaye@isi-mtl.com>
2021-06-24 17:40:25 -0400

    CRUD Controller

--- Microservices/src/main/java/com/ecommerce/microcommerce/controller/ProduitController.java
+++ Microservices/src/main/java/com/ecommerce/microcommerce/controller/ProduitController.java
@@ -1,20 +1,34 @@
 package com.ecommerce.microcommerce.controller;
 
 import com.ecommerce.microcommerce.model.Produit;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.microcommerce.service.ProduitService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class ProduitController {
 
+    @Autowired
+    ProduitService produitService;
+
     @GetMapping("/Produits")
-    public String getProduits(){
-        return "Produit 1";
+    public Iterable<Produit> getProduits(){
+        return produitService.getProducts();
     }
-    @GetMapping("/Produits/{id}")
-    public Produit getPrduit(@PathVariable("id") Long id){
-        return new Produit(1L,"Aspirateur", 200.5);
+    @GetMapping("/Produit/{id}")
+    public Optional<Produit> getPrduit(@PathVariable("id") Long id){
+        return produitService.getProduct(id);
     }
 
+    @DeleteMapping("/Produit/{id}")
+    public void deletePrduit(@PathVariable("id") Long id){
+        produitService.deleteProduct(id);
+    }
+
+    @PostMapping("/Produit")
+    public Produit addProduit(@RequestBody Produit produit){
+        return produitService.saveProduct(produit);
+    }
 }
